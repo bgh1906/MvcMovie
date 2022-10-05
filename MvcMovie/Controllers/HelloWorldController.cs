f2d8e29633f035de85336ca0c96d181c29a0a73d
배지환 <bgh1906@naver.com>
2022-10-05 15:31:56 +0900

    뷰 추가

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -8,20 +8,29 @@ namespace MvcMovie.Controllers
         // 
         // GET: /HelloWorld/
 
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
-        // GET: /HelloWorld/Welcome/ 
-        // Requires using System.Text.Encodings.Web;
 
-        public string Welcome(string name, int ID = 1)
+
+        //// GET: /HelloWorld/Welcome/ 
+        //// Requires using System.Text.Encodings.Web;
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        //}
+        ////호출 url https://localhost:7291/HelloWorld/Welcome/7?name=Rick
+        ////호출값 Hello Rick, ID: 7
+
+
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
-        }
-        //호출 url https://localhost:7291/HelloWorld/Welcome/7?name=Rick
-        //호출값 Hello Rick, ID: 7
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
 
+            return View();
+        }
     }
 }
